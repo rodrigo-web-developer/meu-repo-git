3ce0b43901fd456e507559799f37a09e1b6153a9
Professor <professor@unimar.br>
2026-05-20 20:26:05 -0300

    alterado produto.cs

--- Loja.Core/Models/Produto.cs
+++ Loja.Core/Models/Produto.cs
@@ -9,6 +9,8 @@ namespace Loja.Core.Models
         public string Nome { get; set; }
         [Range(0, 1000)]
         public decimal Preco { get; set; }
+
+        public string Descricao { get; set; }
         [Required]
         public Categoria Categoria { get; set; }
     }
